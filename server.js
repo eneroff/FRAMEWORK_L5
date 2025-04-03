f7431c355e21d30bb47a3c7bbffb54db83d698c1
eneroff <komissarcikdanya@gmail.com>
2025-04-03 10:36:09 +0300

    /

---
+++ server.js
@@ -0,0 +1,23 @@
+const MinimalExpress = require('./app');
+const app = new MinimalExpress();
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello, World!</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.json({ message: 'This is the About page' });
+});
+
+app.post('/data', (req, res) => {
+  res.json({ received: req.body });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
